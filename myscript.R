d0be2d8146ba7da39cd9b6b1f26c5f1640bae455
Zellner <ru32ren@fak11.lmu.de>
2024-01-15 14:55:55 +0100

    added curve fitting

--- myscript.R
+++ myscript.R
@@ -1,2 +1,13 @@
-mydata = read.csv("H:/Methodenschule/git/example_data.csv")
-plot(mydata$xdata,mydata$ydata)
+mydata = read.csv("C:/Users/ru32ren/Documents/first_test_repo/example_data.csv")
+plot(mydata$xdata,mydata$ydata,col='red')
+
+# some guesses for the parameters.
+p1 = 1
+p2 = 0.2
+
+# do the fit
+fit = nls(ydata ~ p1*cos(p2*xdata) + p2*sin(p1*xdata), data = mydata, start = list(p1=p1,p2=p2))
+
+#Plot the fitted line
+new = data.frame(xdata = seq(min(mydata$xdata),max(mydata$xdata),len=200))
+lines(new$xdata,predict(fit,newdata=new))
